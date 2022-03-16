df112b398cbe839153ad61a4e8d26bd929a88d72
Николай <@DESKTOP-8VK6QD8>
2022-03-16 22:07:04 +0300

    Добавьте файлы проекта.

---
+++ laba 1_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace qwerty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем диска: {drive.TotalSize} байт");
+                    Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace} байт");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                    Console.WriteLine($"Тип файловой системы: {drive.DriveFormat}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
